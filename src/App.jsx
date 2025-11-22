f266432da2dd0ba48e8ce0198e04ea60a3d33fdd
rishonkumar <rishon.kumar.rahi@gmail.com>
2025-11-22 21:20:43 +0530

    Example for UseMemo in React

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-import { useEffect, useState } from 'react';
+import { memo, useEffect, useState } from 'react';
 import './App.css'
 import { CreateTodos } from "./components/CreatedTodos.jsx";
 import { Todos } from "./components/Todos.jsx";
@@ -27,6 +27,7 @@ function App() {
     }, 10000);
   }, []);
 
+
   return (
     <>
       {/* <CreateTodos />
@@ -35,8 +36,35 @@ function App() {
       <button onClick={() => setSelectedId(2)}>Todo 2</button>
       <button onClick={() => setSelectedId(3)}>Todo 3</button>
       <Todos id={selectedId} />
+
+      <ButtonComponent />
+      <ButtonComponentWithoutMemo />
     </>
   )
+
+}
+
+// We will useMemo if we dont want to render child component un-necessary basically we will avodi re-render when parents render
+const ButtonComponent = memo(() => {
+  console.log("child render with memo");
+
+  return (
+    <div>
+      <button>Button Clicked</button>
+    </div>
+  )
+  
+})
+
+const ButtonComponentWithoutMemo = () => {
+  console.log("child render without memo");
+
+  return (
+    <div>
+      <button>Button Clicked</button>
+    </div>
+  )
+  
 }
 
 export default App
