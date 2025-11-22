1f5f1070791cce8d135c402dce39d745f77a451f
rishonkumar <rishon.kumar.rahi@gmail.com>
2025-11-22 09:47:16 +0530

    Getting By Id

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,8 +1,26 @@
-export const Todos = ({ todos }) => {
+import { useEffect, useState } from "react"
+
+export const Todos = ({ id }) => {
+
+    const [todo, setTodo] = useState({})
+
+    useEffect(() => {
+        const data = fetch("https://jsonplaceholder.typicode.com/todos/" + id)
+            .then(res => res.json())
+            .then(data => setTodo(data))
+
+    }, [id]) //run again when id changes
+
+
     return (
         <div>
-            <h1>Todos</h1>
-            {todos.map(function (todo) {
+            <h1>Todos #{id}</h1>
+
+            <h1>Title : {todo.title}</h1>
+            <h1>Description : {todo.title}</h1>
+            <h1>Completed : {todo.completed == true ? "Completed" : "Not Completed"}</h1>
+
+            {/* {todos.map(function (todo) {
                 return (
                     <div>
                         <h1>{todo.title}</h1>
@@ -10,7 +28,7 @@ export const Todos = ({ todos }) => {
                         <button>{todo.completed == true ? "Completed" : "Mark as Complete"}</button>
                     </div>
                 )
-            })}
+            })} */}
         </div>
     )
 }
